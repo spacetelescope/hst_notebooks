9153bd005e2f4311843432885bcc8e1059ec5fbb
dulude <dulude@stsci.edu>
2023-11-14 14:25:35 -0500

    make_dependabot_file.py: initial commit.

---
+++ .github/scripts/make_dependabot_file.py
@@ -0,0 +1,28 @@
+"""
+This script finds all paths in notebooks/*/*/ that contain notebook-level requirements.txt files and
+generates the
+"""
+import glob
+import pdb
+
+file_content = ["version: 2",
+                "updates:"]
+
+#1: locate all paths with notebook-level requirements.txt files.
+req_file_list = glob.glob("notebooks/*/*/requirements.txt")
+
+for item in sorted(req_file_list):
+    rf_path = item.replace("requirements.txt", "")
+    template_content = ['  - package-ecosystem: "pip"',
+                        '    directory: "{}"'.format(rf_path),
+                        '    schedule:',
+                        '     interval: "weekly"',
+                        '     day: "sunday"',
+                        '     time: "12:00"',
+                        '    target-branch: dependabot_sandbox']
+    for line in template_content:
+        file_content.append(line)
+
+for line in file_content:
+    print(line)
+
