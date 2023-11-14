d53145fe545520c9c6bd73fd661f26b5dcedeaa6
dulude <dulude@stsci.edu>
2023-11-14 15:42:36 -0500

    minor tweaks to make_dependabot_file.py

--- .github/scripts/make_dependabot_file.py
+++ .github/scripts/make_dependabot_file.py
@@ -1,24 +1,27 @@
 """
-This script finds all paths in notebooks/*/*/ that contain notebook-level requirements.txt files and
-generates the
+This script generates .github/dependabot.yml. It assumes 1) github-actions workflows are stored in
+.github/workflows/ and 2) notebook-level requirements.txt files are found in notebooks/*/*/requirements.txt
 """
-import glob
-import pdb
 
-file_content = ['version: 2',
-                'updates:',
-                '  - package-ecosystem: "github-actions"',
-                '    directory: "/"',
-                '    schedule:',
-                '     interval: "weekly"',
-                '     time: "12:00"',
-                '    target-branch: dependabot_sandbox']
+import glob
+
+output_file_name = ".github/dependabot.yml"
+output_file_content = ['version: 2',
+                       'updates:',
+                       '  - package-ecosystem: "github-actions"',
+                       '    directory: "/"',
+                       '    schedule:',
+                       '     interval: "weekly"',
+                       '     day: "sunday"',
+                       '     time: "12:00"',
+                       '    target-branch: dependabot_sandbox']
 
 #1: locate all paths with notebook-level requirements.txt files.
 req_file_list = glob.glob("notebooks/*/*/requirements.txt")
 
-for item in sorted(req_file_list):
-    rf_path = item.replace("requirements.txt", "")
+# 2: Dynamically generate the dependabot.yml file content based on the paths identified by the above glob command.
+for rf_list_item in sorted(req_file_list):
+    rf_path = rf_list_item.replace("requirements.txt", "")
     template_content = ['  - package-ecosystem: "pip"',
                         '    directory: "{}"'.format(rf_path),
                         '    schedule:',
@@ -27,8 +30,9 @@ for item in sorted(req_file_list):
                         '     time: "12:00"',
                         '    target-branch: dependabot_sandbox']
     for line in template_content:
-        file_content.append(line)
+        output_file_content.append(line)
 
-for line in file_content:
-    print(line)
-
+# 3: Write yml file content
+with open(output_file_name, 'w') as f:
+  f.writelines([line + "\n" for line in output_file_content])
+print("Successfully generated file {}.".format(output_file_name))
