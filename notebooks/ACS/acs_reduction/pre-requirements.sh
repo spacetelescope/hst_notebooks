e1848c6249a55fc6d64eee1b9e1ff18ee9a8befc
M.Gough <60232355+mgough-970@users.noreply.github.com>
2024-08-06 16:06:51 -0400

    Update pre-requirements.sh

--- notebooks/ACS/acs_reduction/pre-requirements.sh
+++ notebooks/ACS/acs_reduction/pre-requirements.sh
@@ -1,6 +1,6 @@
 #! /bin/bash
-source /usr/share/miniconda/etc/profile.d/conda.sh
+#source /usr/share/miniconda/etc/profile.d/conda.sh
 conda create --yes -n hst-env python=3.11 # conda
-conda init bash
-conda activate hst-env
+#conda init bash
+#conda activate hst-env
 conda install --yes -c conda-forge hstcal
