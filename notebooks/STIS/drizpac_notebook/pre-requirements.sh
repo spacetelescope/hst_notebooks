0b5b06a114e03f2730d93491496995366ed56e19
Jackie Brown <jbrown@stsci.edu>
2025-04-19 21:23:52 -0400

    fix_dependencies

--- notebooks/STIS/drizpac_notebook/pre-requirements.sh
+++ notebooks/STIS/drizpac_notebook/pre-requirements.sh
@@ -1,6 +1,4 @@
 # Install numpy
-pip install --force-reinstall -v 'numpy<2.0.0'
-# Install stis_cti
-pip install --no-cache-dir stis_cti
+pip install numpy
 # Install hstcal
-conda install -y -c conda-forge hstcal==2.7.4
+conda install -y -c conda-forge hstcal==3.1.0
